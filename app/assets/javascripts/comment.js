fe5ad1ed3830b1baebe5660586a3079a84306d7d
toumorokoshi96 <chucchuchu2019@icloud.com>
2019-09-27 13:11:02 +0900

    creation of comment into DB

---
+++ app/assets/javascripts/comment.js
@@ -0,0 +1,17 @@
+window.addEventListener('DOMContentLoaded', function(){
+  $('#new_comment').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+  })
+})
+
+
