1771dec73f5fb4ff0b76ca58dc558d664b3ef8b2
toumorokoshi96 <chucchuchu2019@icloud.com>
2019-09-27 13:39:46 +0900

    asynchronous communication has been inplemented

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,4 +1,14 @@
 window.addEventListener('DOMContentLoaded', function(){
+  function buildHTML(comment){
+    var html = `<p>
+                  <strong>
+                    <a href=/users/${comment.user_id}>${comment.user_name}</a>
+                    ：
+                  </strong>
+                  ${comment.text}
+                </p>`
+    return html;
+  }
   $('#new_comment').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -10,6 +20,14 @@ window.addEventListener('DOMContentLoaded', function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.comments').append(html)
+      $('.textbox').val('')
+    })
+    .fail(function(){
+      alert('error');
     })
   })
 })
